53d156417d4572fb1d12ccdd71ce5822ced66598
k250012-nimrah <k250012@nu.edu.pk>
2025-09-17 01:30:23 +0500

    task 2 update

---
+++ labtask2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter a value for b ");//inputting the value for b which will be assigned to a
+    scanf("%d",&a);
+    printf("enter a value for a ");
+
+    scanf("%d",&b   );
+    printf("a=%d\n",a);
+    
+    
+
+    printf("b=%d",b);
+    
+
+
+}
