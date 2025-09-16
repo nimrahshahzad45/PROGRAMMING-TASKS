d6d78c74758335c856d8493cf2e3cfa78f54503e
k250012-nimrah <k250012@nu.edu.pk>
2025-09-17 02:38:38 +0500

    task 5 update

---
+++ labtask5.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    printf("Name:Nimrah\n\n");
+    printf("AGE:19 \n\n");
+    printf("Couse:\"c programming\"\n\n");
+    printf("Result:Passed");
+    return 0;
+}
